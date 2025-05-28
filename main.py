25eb6d5bce6d26c13cb1f48323cc780d4b1dde9b
Aziz <Abdulaziz392007@gmail.com>
2025-05-28 22:47:59 +0400

    Face recgoniction working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 if not cap.isOpened():
     print("Error: Could not open video device.")
@@ -9,8 +10,14 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    cv2.imshow('frame', frame)
+    
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces= face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+    cv2.imshow('Face Detection App', frame)
     # Press 'q' to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
