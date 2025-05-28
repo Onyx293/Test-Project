06b72ffef31abdeacfda21eab374d7369c2eb773
Aziz <Abdulaziz392007@gmail.com>
2025-05-28 23:23:45 +0400

    Smile recogniction working

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+regiesterd_faces={}
+
 
 if not cap.isOpened():
-    print("Error: Could not open video device.")
+    print("Error: Could not open video camera   ")
     exit()
 
 while True:
@@ -15,9 +18,27 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces= face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 225, 0), 2)
+        cv2.putText(frame, 'Face Detected', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+
+
+    for i, (x,y,w,h) in enumerate(faces):
+        face_roi_gray = gray[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(face_roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(25, 25))
+        regiesterd_faces[i] = (x, y, w, h)
+
+        if len(smiles) > 0:
+            cv2.putText(frame, 'Smile Detected', (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        else:
+            cv2.putText(frame, 'No Smile Detected', (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+        
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.putText(frame, ("faces deteced: " + str(len(faces))), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow('Face Detection App', frame)
+    
     # Press 'q' to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
